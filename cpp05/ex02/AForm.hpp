35d283b8b46d4df89491f82dfd71020f8deaaa3b
ibrahim berat <gursesberat0@gmail.com>
2026-01-20 00:31:54 +0300

    added code on ex02

---
+++ cpp05/ex02/AForm.hpp
@@ -0,0 +1,51 @@
+
+#ifndef AFORM_HPP
+#define AFORM_HPP
+
+#include <iostream>
+#include <string>
+
+class Bureaucrat;
+
+class AForm
+{
+    private:
+        const std::string name;
+        bool isSigned;
+        const int gradeToSign;
+        const int gradeToExecute;
+	protected:
+		virtual void executeAction() const = 0;
+    public:
+        AForm();
+        AForm(const std::string &name, int gradeToSign, int gradeToExecute);  
+        AForm(const AForm &other);
+        AForm &operator=(const AForm &other);
+        ~AForm();
+        
+        class GradeTooHighException : public std::exception
+        {
+            public:
+                virtual const char* what() const throw();
+        };
+        class GradeTooLowException : public std::exception
+        {
+            public:
+                virtual const char* what() const throw();
+        };
+		class FormNotSignedException : public std::exception 
+		{
+			public:
+           		virtual const char* what() const throw();
+    	};	
+        void beSigned(const Bureaucrat &bureaucrat);
+        const std::string &getName() const;
+        bool getIsSigned() const;
+        int getGradeToSign() const;
+        int getGradeToExecute() const;
+		void execute(const Bureaucrat &executor) const;
+};
+
+std::ostream &operator<<(std::ostream &out, const AForm &f);
+
+#endif
