b874d95b7aa7f655ab77c5161e369d40bae88838
ibrahim berat <gursesberat0@gmail.com>
2026-02-07 23:20:45 +0300

    added cpp06-ex00

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -6,11 +6,18 @@
 /*   By: igurses <igurses@student.42istanbul.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/03 15:52:58 by igurses           #+#    #+#             */
-/*   Updated: 2026/02/03 16:27:02 by igurses          ###   ########.fr       */
+/*   Updated: 2026/02/07 20:34:31 by igurses          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ScalarConverter.hpp"  
+#include "ScalarConverter.hpp"
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <climits>
+#include <cerrno>
+#include <cmath>
+#include <iomanip>
 
 ScalarConverter::ScalarConverter(){}
 
@@ -26,9 +33,155 @@ ScalarConverter& ScalarConverter::operator=(const ScalarConverter& other)
 
 ScalarConverter::~ScalarConverter() {}
 
+static int getType(const std::string& literal)
+{
+    if (literal.length() == 3 && literal[0] == '\'' && literal[2] == '\'')
+        return 0; 
+    
+    if (literal == "nan" || literal == "nanf" || 
+        literal == "+inf" || literal == "-inf" || 
+        literal == "+inff" || literal == "-inff" ||
+        literal == "inf" || literal == "inff")
+        return 3;
+    
+    if (literal[literal.length() - 1] == 'f')
+        return 2; 
+    
+    bool hasDecimal = false;
+    size_t i = 0;
+    if (literal[0] == '+' || literal[0] == '-')
+        i = 1;
+    for (; i < literal.length(); i++)
+    {
+        if (literal[i] == '.')
+            hasDecimal = true;
+        else if (!isdigit(literal[i]))
+            return -1;
+    }
+    if (hasDecimal)
+        return 3;
+    else
+        return 1;
+}
+
+static void printChar(double value)
+{
+    std::cout << "char: ";
+    if (std::isnan(value) || std::isinf(value))
+        std::cout << "impossible";
+    else if (value < 0 || value > 127)
+        std::cout << "impossible";
+    else if (value < 32 || value == 127)
+        std::cout << "Non displayable";
+    else
+        std::cout << "'" << static_cast<char>(value) << "'";
+    std::cout << std::endl;
+}
+
+static void printInt(double value)
+{
+    std::cout << "int: ";
+    if (std::isnan(value) || std::isinf(value))
+        std::cout << "impossible";
+    else if (value > INT_MAX || value < INT_MIN)
+        std::cout << "impossible";
+    else
+        std::cout << static_cast<int>(value);
+    std::cout << std::endl;
+}
+
+static void printFloat(double value)
+{
+    std::cout << "float: ";
+    if (std::isnan(value))
+        std::cout << "nanf";
+    else if (std::isinf(value))
+    {
+        if (value > 0)
+            std::cout << "+inff";
+        else
+            std::cout << "-inff";
+    }
+    else
+    {
+        float f = static_cast<float>(value);
+        std::cout << std::fixed << std::setprecision(1) << f << "f";
+    }
+    std::cout << std::endl;
+}
+
+static void printDouble(double value)
+{
+    std::cout << "double: ";
+    if (std::isnan(value))
+        std::cout << "nan";
+    else if (std::isinf(value))
+    {
+        if (value > 0)
+            std::cout << "+inf";
+        else
+            std::cout << "-inf";
+    }
+    else
+        std::cout << std::fixed << std::setprecision(1) << value;
+    std::cout << std::endl;
+}
 
 void ScalarConverter::convert(const std::string& literal)
-{
-   std::cout << "Converting literal: " << literal << std::endl;
-   
+{           
+    if (literal.empty())
+    {
+        std::cerr << "Error: Empty literal" << std::endl;
+        return;
+    }
+    int type = getType(literal);
+    double value = 0;
+
+    switch (type)
+    {
+    case 0:
+        value = static_cast<double>(literal[1]);
+        break;
+    case 1:
+    {
+        char* endptr;
+        long tmp = std::strtol(literal.c_str(), &endptr, 10);
+        if (*endptr != '\0' || errno == ERANGE)
+        {
+            std::cout << "Error: invalid literal" << std::endl;
+            return;
+        }
+        value = static_cast<double>(tmp);
+    }
+        break;
+    case 2:
+        {
+            char* endptr;
+            value = std::strtod(literal.c_str(), &endptr);
+            if (endptr == literal.c_str() || (*endptr != 'f' && *endptr != '\0'))
+            {
+                std::cout << "Error: invalid literal" << std::endl;
+                return;
+            }
+        }
+        break;
+    case 3:
+    {
+        char* endptr;
+        value = std::strtod(literal.c_str(), &endptr);
+        if (endptr == literal.c_str())
+        {
+            std::cout << "Error: invalid literal" << std::endl;
+            return;
+        }
+    }
+        break;
+    default:
+        std::cout << "Error: invalid literal" << std::endl;
+        return;
+    }
+    printChar(value);
+    printInt(value);
+    printFloat(value);
+    printDouble(value);
 }
--- cpp06/ex00/ScalarConverter.hpp
+++ cpp06/ex00/ScalarConverter.hpp
@@ -6,26 +6,31 @@
 /*   By: igurses <igurses@student.42istanbul.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/03 14:50:54 by igurses           #+#    #+#             */
-/*   Updated: 2026/02/03 15:51:14 by igurses          ###   ########.fr       */
+/*   Updated: 2026/02/07 20:34:01 by igurses          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SCALARCONVERTER_HPP
 #define SCALARCONVERTER_HPP
 
-
-
+#include <iostream>
+#include <string>
+#include <limits>
+#include <cmath>
+#include <cstdlib> 
+#include <climits>   
+#include <cerrno>   
+#include <iomanip>    
 
 class ScalarConverter
 {
-    public:
-        ScalarConverter();
+    private:
+        ScalarConverter(); // çünkü bu sınıfın nesnesi oluşturulmayacak, sadece static fonksiyonları kullanılacak
         ScalarConverter(const ScalarConverter& other);
         ScalarConverter& operator=(const ScalarConverter& other);
         ~ScalarConverter();     
-        static void convert(const std::string& literal);   
+    public:
+        static void convert(const std::string& literal);   // static fonksiyon, nesne oluşturmadan çağrılabilir
  };
-
-
  
 #endif 
