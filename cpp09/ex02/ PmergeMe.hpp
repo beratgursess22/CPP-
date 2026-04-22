905b03e70a3c777e6d53b9bb7d5f30b70092af6a
ibrahim berat <gursesberat0@gmail.com>
2026-04-22 14:10:20 +0300

    added main.cpp

--- cpp09/ex02/ PmergeMe.hpp
+++ cpp09/ex02/ PmergeMe.hpp
@@ -8,6 +8,7 @@
 # include <sstream>
 # include <stdexcept>
 # include <vector>
+#include <iostream>
 
 class PmergeMe
 {
--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -6,7 +6,28 @@
 /*   By: igurses <igurses@student.42istanbul.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/22 14:05:21 by igurses           #+#    #+#             */
-/*   Updated: 2026/04/22 14:05:22 by igurses          ###   ########.fr       */
+/*   Updated: 2026/04/22 14:10:05 by igurses          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "PmergeMe.hpp"
+
+int main (int argc, char **argv)
+{
+	if (argc < 2)
+	{
+		std::cerr << "Usage: " << argv[0] << " <numbers>" << std::endl;
+		return 1;
+	}
+	try
+	{
+		PmergeMe merge(argv + 1);
+		merge.execute();
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+		return 1;
+	}
+	return 0;
+}
